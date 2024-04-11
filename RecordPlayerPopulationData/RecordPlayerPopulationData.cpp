174c37d566b87ef74f39990b3c3b6e32e83bcad9
mgavin <matthewgavin489@gmail.com>
2024-04-11 13:49:20 -0500

    Working state

--- RecordPlayerPopulationData/RecordPlayerPopulationData.cpp
+++ RecordPlayerPopulationData/RecordPlayerPopulationData.cpp
@@ -1,5 +1,4 @@
 #include "RecordPlayerPopulationData.h"
-#include <format>
 #include "HookedEvents.h"
 #include "Logger.h"
 
@@ -18,6 +17,71 @@ void RecordPlayerPopulationData::onLoad() {
 
         // so tired...
         LOG("{}", last_time);
+
+        using namespace std::chrono_literals;
+        std::chrono::system_clock clk;
+        if ((clk.now() - last_time) < 5min) {
+                LOG("TOO SOON!");
+        }
+
+        cvarManager->registerCvar(
+                "rppd_enable_topscroll",
+                "0",
+                "Flag to determine showing the top ticker scroll showing player population");
+
+        cvarManager->registerCvar("rppd_auto_enable", "0", "Flag to determine if matchmaking should be automatic");
+
+        HookedEvents::AddHookedEvent("Function Engine.Actor.Timer", [this](std::string ev) {
+                // THIS IS ACTUALLY A SECONDS TIMER
+                if (seconds_counter > 1e6)  // preventing overflow. just cuz.
+                        seconds_counter = 0;
+                seconds_counter++;
+                bool               auto_enable = cvarManager->getCvar("rppd_auto_enable").getBoolValue();
+                MatchmakingWrapper mw          = gameWrapper->GetMatchmakingWrapper();
+                if (mw && auto_enable) {
+                        // the one second delay between searching isn't necessary
+                        // (no wait time is necessary)
+                        // to trigger the "get playlist populations" function running every 5 seconds.
+                        // now to test how long it lasts
+                        if (seconds_counter % 300 == 0) {
+                                mw.StartMatchmaking(PlaylistCategory::CASUAL);
+                                mw.CancelMatchmaking();
+                        }
+                }
+
+                // MAYBE THERE'S ACTUALLY ANOTHER FUNCTION?
+        });
+        // cvarManager->registerNotifier(
+        //         "RPPD_CHECKNOW",
+        //         [this](std::vector<std::string> args) { CHECK_NOW(); },
+        //         "CHECK, RIGHT NOW, THE PLAYER POPULATION!",
+        //         NULL);
+
+        HookedEvents::AddHookedEvent(
+                "Function ProjectX.OnlineGamePopulation_X.GetPlaylistPopulations",
+                // I would like to know how long this lasts.
+                // this seems to be a thing that bakkesmod does? but I can't figure that out necessarily
+                // (aka running the function scanner) without bakkesmod running
+
+                // like, this doesn't even start getting called every 5 seconds until you queue for a game
+
+                [this](std::string ev) {
+                        LOG("getplaylistpopulations: seconds counter: {}", seconds_counter);
+                        MatchmakingWrapper mw = gameWrapper->GetMatchmakingWrapper();
+                        if (mw) {
+                                int tmpp = mw.GetTotalPlayersOnline();
+                                if (TOTAL_POP != tmpp) {
+                                        TOTAL_POP = tmpp;
+                                        LOG("POPULATION UPDATED AT TIME: {}, TIMES UPDATED: {},  POPULATION: {}",
+                                            get_current_datetime_str(),
+                                            updated_count,
+                                            TOTAL_POP);
+                                        updated_count++;
+
+                                        write_population();
+                                }
+                        }
+                });
 }
 
 /// <summary>
@@ -40,6 +104,16 @@ void RecordPlayerPopulationData::RenderSettings() {
         bool        top_scroller = ticker.getBoolValue();
         if (ImGui::Checkbox("Enable ticker scrolling at the top?", &top_scroller)) {
                 ticker.setValue(top_scroller);
+        }
+
+        CVarWrapper auto_enable  = cvarManager->getCvar("rppd_auto_enable");
+        bool        bauto_enable = auto_enable.getBoolValue();
+        if (ImGui::Checkbox("Enable auto matchmaking refresh every 300 seconds?", &bauto_enable)) {
+                auto_enable.setValue(bauto_enable);
+        }
+
+        if (ImGui::Button("CHECK NOW")) {
+                gameWrapper->Execute([this](GameWrapper * gw) { CHECK_NOW(); });
         }
 }
 
@@ -70,11 +144,15 @@ void RecordPlayerPopulationData::init_datafile() {
         }
         if (!std::filesystem::exists(RECORD_POPULATION_FILE) || std::filesystem::is_empty(RECORD_POPULATION_FILE)) {
                 csv::CSVWriter<std::ofstream> recordwriter {file};
-                std::vector<std::string>      header {"DATETIME"};
-                header.emplace_back(
-                        begin(bmhelper::playlist_ids_str),
-                        end(bmhelper::playlist_ids_str),
-                        [](const auto & ele) -> std::string { return ele.second; });
+                std::vector<std::string>      header {"DATETIME", "TOTALPOPULATION", "TOTALPLAYERSONLINE"};
+                // header.emplace_back(
+                //         begin(bmhelper::playlist_ids_str),
+                //         end(bmhelper::playlist_ids_str),
+                //         [](const auto & ele) -> std::string { return ele.second; });
+                // I WANTED THIS TO BE IN A SIMPLER FUNCTION THAT ALREADY EXISTED
+                // LIKE A CUSTOM EMPLACE_BACK
+                auto                          vv = std::views::values(bmhelper::playlist_ids_str);
+                std::copy(begin(vv), end(vv), std::back_inserter(header));
                 recordwriter << header;
         } else {
                 csv::CSVReader recordreader {RECORD_POPULATION_FILE.string()};
@@ -83,13 +161,66 @@ void RecordPlayerPopulationData::init_datafile() {
                 // go to last row
                 while (recordreader.read_row(row))
                         ;
-                last_time = row["DATETIME"].get<std::chrono::time_point<std::chrono::system_clock>>();
+                // last_time = row["DATETIME"].get<std::chrono::time_point<std::chrono::system_clock>>();
+                std::string tmpdt = row["DATETIME"].get<std::string>();
+                last_time         = get_timepoint_from_str(tmpdt);
         }
 }
 
-std::string get_current_datetime() {
-        std::chrono::system_clock clock;
-        return std::format("{0:%F}T{0:%R%z}", clock.now());
+void RecordPlayerPopulationData::CHECK_NOW() {
+        MatchmakingWrapper mw = gameWrapper->GetMatchmakingWrapper();
+        if (mw) {
+                mw.StartMatchmaking(PlaylistCategory::CASUAL);
+                mw.CancelMatchmaking();
+        }
+}
+
+void RecordPlayerPopulationData::write_population() {
+        std::ofstream                 file {RECORD_POPULATION_FILE, std::ios::app};
+        csv::CSVWriter<std::ofstream> recordwriter {file};
+        MatchmakingWrapper            mw = gameWrapper->GetMatchmakingWrapper();
+        if (mw) {
+                std::vector<int> counts;
+                counts.push_back(mw.GetTotalPopulation());
+                counts.push_back(mw.GetTotalPlayersOnline());
+                for (const auto & element : bmhelper::playlist_ids_str) {
+                        counts.push_back(mw.GetPlayerCount(element.first));
+                }
+
+                std::vector<std::string> strs;
+                strs.reserve(bmhelper::playlist_ids_str.size() + 1);
+                strs.push_back(get_current_datetime_str());
+                std::transform(
+                        begin(counts),
+                        end(counts),
+                        std::back_inserter(strs),
+                        [](const auto & elem) -> std::string { return std::to_string(elem); });
+
+                recordwriter << strs;
+        }
+}
+
+/// <summary>
+/// RETURNS THE DATETIME AS A STRING!
+/// </summary>
+/// <returns>_now_ represented as a datetime string</returns>
+std::string RecordPlayerPopulationData::get_current_datetime_str() {
+        const std::chrono::zoned_time t {std::chrono::current_zone(), std::chrono::system_clock::now()};
+        return std::vformat(DATETIME_FORMAT_STR, std::make_format_args(t));
+}
+
+/// <summary>
+/// RETURNS THE TIMEPOINT AS INTERPRETED FROM A DATETIME STRING!
+/// </summary>
+/// <param name="str">a string representation of the datetime</param>
+/// <returns>a time_point representing the string</returns>
+std::chrono::time_point<std::chrono::system_clock> RecordPlayerPopulationData::get_timepoint_from_str(std::string str) {
+        std::chrono::utc_time<std::chrono::system_clock::duration> tmpd;
+        std::istringstream                                         ss(str);
+        ss >> std::chrono::parse(DATETIME_PARSE_STR, tmpd);
+        std::chrono::time_point<std::chrono::system_clock, std::chrono::system_clock::duration> tmptp {
+                tmpd.time_since_epoch()};
+        return tmptp;
 }
 
 /*
--- RecordPlayerPopulationData/RecordPlayerPopulationData.h
+++ RecordPlayerPopulationData/RecordPlayerPopulationData.h
@@ -1,12 +1,27 @@
 #pragma once
 #include <chrono>
+#include <format>
 #include <fstream>
+#include <ranges>
 #include "bakkesmod/imgui/imgui.h"
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
 #include "bmhelper.h"
 #include "csv.hpp"
 #include "HookedEvents.h"
+
+/*
+ * SAMPLE:
+ *
+ * 13219:[19:48:07] [bakkesmod] [class RecordPlayerPopulationData] POPULATION UPDATED AT TIME:
+ 2024-04-08T00:48:07.8930216+0000, TIMES UPDATED: 0,  POPULATION: 326275
+ * 13272:[19:49:46] [bakkesmod] [class RecordPlayerPopulationData] POPULATION UPDATED AT TIME:
+ 2024-04-08T00:49:46.8816692+0000, TIMES UPDATED: 1,  POPULATION: 325712
+ * 13426:[19:54:48] [bakkesmod] [class RecordPlayerPopulationData] POPULATION UPDATED AT TIME:
+ 2024-04-08T00:54:48.8792777+0000, TIMES UPDATED: 2,  POPULATION: 324136
+ * 13578:[19:59:50] [bakkesmod] [class RecordPlayerPopulationData] POPULATION UPDATED AT TIME:
+ 2024-04-08T00:59:50.8810702+0000, TIMES UPDATED: 3,  POPULATION: 322139
+ */
 
 class RecordPlayerPopulationData :
         public BakkesMod::Plugin::BakkesModPlugin,
@@ -16,8 +31,17 @@ private:
 
         const std::filesystem::path RECORD_POPULATION_FILE =
                 gameWrapper->GetDataFolder().append("RecordPopulationData.csv");
+        const std::string DATETIME_FORMAT_STR = "{0:%F}T{0:%T%z}";
+        const std::string DATETIME_PARSE_STR  = "%FT%T%z";
 
-        void init_datafile();
+        void                                               init_datafile();
+        void                                               CHECK_NOW();
+        void                                               write_population();
+        std::string                                        get_current_datetime_str();
+        std::chrono::time_point<std::chrono::system_clock> get_timepoint_from_str(std::string);
+        int                                                TOTAL_POP       = 0;
+        int                                                updated_count   = 1;
+        int                                                seconds_counter = 0;
 
 public:
         void onLoad() override;
